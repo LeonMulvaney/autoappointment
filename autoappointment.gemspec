30c53f7ac53d24b9091fc7155f8a0aec8347e6ec
LeonMulvaney <leonmul96@gmail.com>
2018-04-20 22:39:53 +0100

    Changes for install

--- autoappointment.gemspec
+++ autoappointment.gemspec
@@ -9,14 +9,14 @@ Gem::Specification.new do |spec|
   spec.authors       = ["LeonMulvaney"]
   spec.email         = ["leonmul96@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "Automatically generates an appointment if a patient is issued a follow up visit"
+  spec.description   = "Auto Schedules an appointment if after the prescrion duration is complete"
+  spec.homepage      = ""
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata["allowed_push_host"] = "Set to 'http://mygemserver.com'"
   else
     raise "RubyGems 2.0 or newer is required to protect against " \
       "public gem pushes."
--- lib/autoappointment.rb
+++ lib/autoappointment.rb
@@ -1,5 +1,45 @@
 require "autoappointment/version"
 
 module Autoappointment
-  # Your code goes here...
+ def self.generate(prescription) #Method titled check to estimate which ward the patient should be allocated
+      #Save parameters as instance variables
+    @prescription = prescription
+   	@follow_up = @prescription.follow_up
+    #Auto Generate Appointment based on prescription
+    #Adding Records From: https://stackoverflow.com/questions/12490076/rails-3-how-to-insert-record-in-database-using-rails
+    #Adding Days to Date From: https://stackoverflow.com/questions/4654457/how-to-add-10-days-to-current-time-in-rails
+    #Checking a day via Date From: https://stackoverflow.com/questions/11914422/ruby-check-if-date-is-a-weekend
+
+	#If follow up is True, create new record and return True
+    if @follow_up = "True" 
+	    @duration = @prescription.duration
+	    @prescription_date = @prescription.date
+	    @new_visit_date = Date.parse(@prescription_date) + @duration #Get date and add the duration i.e. 7 days 
+
+		#Check if the new appointment day is a weekend, if so re-schedule for the following Monday
+	    if @new_visit_date.saturday? 
+	    	@new_visit_date = @new_visit_date +2 # If Saturday (True), add 2 days to the orignally scheduled date
+
+	    elsif @new_visit_date.sunday?
+	    	@new_visit_date = @new_visit_date +1 #If Sunday (True), add 1 day to the originally scheduled date
+	    else
+	    	#Do nothing 
+	    end
+
+	    @new_visit_date_as_string = @new_visit_date.to_s #Parse date back into string before for Database Entry
+	    @patient_name = @prescription.patient_name #Get name to search for below
+
+	    @time = "09:30 AM" #Default time
+	    @visited = "False" # Set visited to False for Appointment Record
+    	@appointment = Appointment.new(:date => @new_visit_date_as_string, :time =>@time, :patient => @patient_name, :visited =>@visited) #Create the Appointment using prescription parameters
+     	@appointment.save
+    	@result = "True" #Set return statement to true
+   
+    else
+    	@result = "False" #Return False if follow_up is False
+    end
+
+    return @result #Return value from Gem back to Controller 
+
+ end
 end
